d5ca6b92daecd87d08a1353e43a2cd5a37d007ff
MarcoSalas <marco_salas@live.com.mx>
2017-11-17 14:40:22 -0600

    Adding Factorial, FindNumber y avance IsPalindrome

---
+++ IsPalindrome.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+ 
+def IsPalindrome(Text):
+
+ 
+ if __name__ == '__main__':
+
+     print('Veremos si una palabra u oración es palíndroma')
+
+     Word = str(input('Escribe una palabra o una oración: '))
+
+     Res = IsPalindrome(Word)
